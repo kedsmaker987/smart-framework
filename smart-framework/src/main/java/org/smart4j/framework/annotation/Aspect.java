aff8016f18b0c85b9aca082100b596f7a9e3f7e2
kedsmaker987 <kedsmake@163.com>
2018-01-27 00:20:34 +0800

    修正Aspect注解不能使用问题

--- smart-framework/src/main/java/org/smart4j/framework/annotation/Aspect.java
+++ smart-framework/src/main/java/org/smart4j/framework/annotation/Aspect.java
@@ -2,6 +2,8 @@ package org.smart4j.framework.annotation;
 
 import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -10,6 +12,7 @@ import java.lang.annotation.Target;
  *
  */
 @Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Aspect {
 	
 	Class<? extends Annotation> value();
